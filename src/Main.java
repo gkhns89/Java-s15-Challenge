3c6aa91ed372b1f210b7ea0a2b7d6dcf79d1d8ca
Gökhan <gokhansisman@outlook.com>
2023-12-13 21:27:32 +0300

    Little test!

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import com.angel.library.enums.GenderType;
 import com.angel.library.enums.MemberCategories;
 import com.angel.library.model.Member;
 
+
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Member x = new Member("123","Gökhan","Şişman", GenderType.MALE, new ArrayList<>(),1, MemberCategories.STUDENT);
-
+        Scanner scanner = new Scanner("Welcome to Angel Library v1.0.1");
 
 
     }
