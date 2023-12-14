7ed3930c47e220087c5f3e1a052abf9a1df6cbe4
Gökhan <gokhansisman@outlook.com>
2023-12-14 03:44:42 +0300

    Making animated welcome message with ASCIIart!

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 import com.angel.library.enums.GenderType;
 import com.angel.library.enums.MemberCategories;
+import com.angel.library.model.AsciiArt;
 import com.angel.library.model.Member;
-
-
-import java.io.Reader;
+import java.awt.*;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
 
-        Member x = new Member("123","Gökhan","Şişman", GenderType.MALE, new ArrayList<>(),1, MemberCategories.STUDENT);
-        Scanner scanner = new Scanner("Welcome to Angel Library v1.0.1");
+        Member x = new Member("123", "Gökhan", "Şişman", GenderType.MALE, new ArrayList<>(), 1, MemberCategories.STUDENT);
+
+        AsciiArt asciiArt = new AsciiArt();
+        String text = "ANGEL  LIBRARY  v1.0.1";
+        AsciiArt.Settings settings = new AsciiArt.Settings(new Font("SansSerif", Font.PLAIN, 18), text.length() * 30, 30);
+
+        asciiArt.drawString(text, "*", settings);
+
+        System.out.println("\nWelcome to ANGEL LIBRARY v1.0.1");
 
 
     }
